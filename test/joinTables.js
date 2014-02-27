57058f24313244536323fa86f3288e53fd206bd6
Cody Stoltman <particlebanana@gmail.com>
2014-02-27 16:28:51 -0600

    don't require dominant on self-referencing associations

--- test/joinTables.js
+++ test/joinTables.js
@@ -148,4 +148,63 @@ describe('JoinTables', function() {
     });
   });
 
+
+  describe('self-referencing associations', function() {
+    var collections = {};
+
+    before(function() {
+
+      collections.foo = {
+        tableName: 'foo',
+        connection: 'bar',
+        attributes: {
+          id: {
+            type: 'integer',
+            autoIncrement: true,
+            primaryKey: true,
+            unique: true
+          },
+
+          isFollowing: {
+            collection: 'foo',
+            via: 'followedBy'
+          },
+
+          followedBy: {
+            collection: 'foo',
+            via: 'isFollowing'
+          }
+        }
+      };
+    });
+
+    it('should add a junction table for the self referencing attributes', function() {
+      var obj = new JoinTables(collections);
+
+      assert(obj.foo_followedby__foo_isfollowing);
+      assert(!obj.foo_isfollowing__foo_followedby);
+
+      assert(obj.foo_followedby__foo_isfollowing.attributes.foo_isFollowing);
+      assert(obj.foo_followedby__foo_isfollowing.attributes.foo_isFollowing.type === 'integer');
+      assert(obj.foo_followedby__foo_isfollowing.attributes.foo_isFollowing.columnName === 'foo_isFollowing');
+      assert(obj.foo_followedby__foo_isfollowing.attributes.foo_isFollowing.foreignKey === true);
+      assert(obj.foo_followedby__foo_isfollowing.attributes.foo_isFollowing.references === 'foo');
+      assert(obj.foo_followedby__foo_isfollowing.attributes.foo_isFollowing.on === 'id');
+      assert(obj.foo_followedby__foo_isfollowing.attributes.foo_isFollowing.groupKey === 'foo');
+
+      assert(obj.foo_followedby__foo_isfollowing.attributes.foo_followedBy);
+      assert(obj.foo_followedby__foo_isfollowing.attributes.foo_followedBy.type === 'integer');
+      assert(obj.foo_followedby__foo_isfollowing.attributes.foo_followedBy.columnName === 'foo_followedBy');
+      assert(obj.foo_followedby__foo_isfollowing.attributes.foo_followedBy.foreignKey === true);
+      assert(obj.foo_followedby__foo_isfollowing.attributes.foo_followedBy.references === 'foo');
+      assert(obj.foo_followedby__foo_isfollowing.attributes.foo_followedBy.on === 'id');
+      assert(obj.foo_followedby__foo_isfollowing.attributes.foo_followedBy.groupKey === 'foo');
+
+      assert(obj.foo.attributes.isFollowing.references === 'foo_followedby__foo_isfollowing');
+      assert(obj.foo.attributes.isFollowing.on === 'foo_isFollowing');
+      assert(obj.foo.attributes.followedBy.references === 'foo_followedby__foo_isfollowing');
+      assert(obj.foo.attributes.followedBy.on === 'foo_followedBy');
+    });
+  });
+
 });
