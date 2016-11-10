14ffa9803f332cd051ce7aa2822daed97e94ec93
Cody Stoltman <particlebanana@gmail.com>
2016-11-10 16:31:40 -0600

    refactor the foreign key mapper to better reflect updates in waterline 0.13

--- test/foreignKeys.js
+++ test/foreignKeys.js
@@ -1,28 +1,45 @@
-var ForeignKeys = require('../lib/waterline-schema/foreignKeys'),
-    assert = require('assert');
+var assert = require('assert');
+var _ = require('@sailshq/lodash');
+var SchemaBuilder = require('../lib/waterline-schema/schema');
+var ForeignKeyMapper = require('../lib/waterline-schema/foreignKeys');
 
-describe('ForeignKeys', function() {
-
-  describe('with automatic column names', function() {
-    var collections = {};
+describe('Foreign Key Mapper :: ', function() {
+  describe('With automatic column name properties', function() {
+    var schema;
 
     before(function() {
-      collections.foo = {
-        attributes: {
-          id: {
-            type: 'integer',
-            autoIncrement: true,
-            primaryKey: true,
-            unique: true
+      var fixtures = [
+        {
+          identity: 'foo',
+          primaryKey: 'id',
+          attributes: {
+            id: {
+              type: 'string'
+            }
+          }
+        },
+        {
+          identity: 'bar',
+          primaryKey: 'id',
+          attributes: {
+            id: {
+              type: 'number'
+            },
+            foo: {
+              model: 'foo'
+            }
           }
         }
-      };
+      ];
 
-      collections.bar = {
-        attributes: {
-          foo: { model: 'foo' }
-        }
-      };
+      var collections = _.map(fixtures, function(obj) {
+        var collection = function() {};
+        collection.prototype = obj;
+        return collection;
+      });
+
+      // Build the schema
+      schema = SchemaBuilder(collections);
     });
 
     /**
@@ -31,7 +48,7 @@ describe('ForeignKeys', function() {
      * attributes: {
      *   foo: {
      *     columnName: 'foo',
-     *     type: 'integer',
+     *     type: 'number',
      *     foreignKey: true,
      *     references: 'foo',
      *     on: 'id'
@@ -40,39 +57,57 @@ describe('ForeignKeys', function() {
      */
 
     it('should add a foreign key mapping to the bar collection', function() {
-      var obj = new ForeignKeys(collections);
-      assert(obj.bar.attributes.foo);
-      assert(obj.bar.attributes.foo.columnName === 'foo');
-      assert(obj.bar.attributes.foo.foreignKey === true);
-      assert(obj.bar.attributes.foo.references === 'foo');
-      assert(obj.bar.attributes.foo.on === 'id');
+      // Map out the foreign keys
+      ForeignKeyMapper(schema);
+      var barSchema = schema.bar.schema;
+
+      assert.equal(barSchema.foo.columnName, 'foo');
+      assert.equal(barSchema.foo.type, 'string');
+      assert.equal(barSchema.foo.foreignKey, true);
+      assert.equal(barSchema.foo.references, 'foo');
+      assert.equal(barSchema.foo.on, 'id');
     });
   });
 
   describe('with custom column names', function() {
-    var collections = {};
+    var schema;
 
     before(function() {
-      collections.foo = {
-        attributes: {
-          id: {
-            type: 'integer',
-            autoIncrement: true,
-            primaryKey: true,
-            unique: true
+      var fixtures = [
+        {
+          identity: 'foo',
+          primaryKey: 'id',
+          attributes: {
+            id: {
+              type: 'string'
+            }
+          }
+        },
+        {
+          identity: 'bar',
+          primaryKey: 'id',
+          attributes: {
+            id: {
+              type: 'number'
+            },
+            foo: {
+              model: 'foo',
+              columnName: 'xyz_foo_id'
+            }
           }
         }
-      };
+      ];
 
-      collections.bar = {
-        attributes: {
-          foo: {
-            columnName: 'xyz_foo_id',
-            model: 'foo'
-          }
-        }
-      };
+      var collections = _.map(fixtures, function(obj) {
+        var collection = function() {};
+        collection.prototype = obj;
+        return collection;
+      });
+
+      // Build the schema
+      schema = SchemaBuilder(collections);
     });
+
 
     /**
      * Test that a foreign key gets built for the bar table in the following structure:
@@ -89,57 +124,15 @@ describe('ForeignKeys', function() {
      */
 
     it('should add a foreign key mapping to the custom column name', function() {
-      var obj = new ForeignKeys(collections);
-      assert(obj.bar.attributes.foo);
-      assert(obj.bar.attributes.foo.columnName === 'xyz_foo_id');
-      assert(obj.bar.attributes.foo.foreignKey === true);
-      assert(obj.bar.attributes.foo.references === 'foo');
-      assert(obj.bar.attributes.foo.on === 'id');
-    });
-  });
+      // Map out the foreign keys
+      ForeignKeyMapper(schema);
+      var barSchema = schema.bar.schema;
 
-  describe('with custom primary key', function() {
-    var collections = {};
-
-    before(function() {
-      collections.foo = {
-        attributes: {
-          uuid: {
-            type: 'string',
-            primaryKey: true
-          }
-        }
-      };
-
-      collections.bar = {
-        attributes: {
-          foo: { model: 'foo' }
-        }
-      };
-    });
-
-    /**
-     * Test that a foreign key gets built for the bar table in the following structure:
-     *
-     * attributes: {
-     *   foo: {
-     *     columnName: 'foo_uuid',
-     *     type: 'integer',
-     *     foreignKey: true,
-     *     references: 'foo',
-     *     on: 'id'
-     *   }
-     * }
-     */
-
-    it('should add a foreign key mapping to the custom column name', function() {
-      var obj = new ForeignKeys(collections);
-      assert(obj.bar.attributes.foo);
-
-      assert(obj.bar.attributes.foo.columnName === 'foo');
-      assert(obj.bar.attributes.foo.foreignKey === true);
-      assert(obj.bar.attributes.foo.references === 'foo');
-      assert(obj.bar.attributes.foo.on === 'uuid');
+      assert.equal(barSchema.foo.columnName, 'xyz_foo_id');
+      assert.equal(barSchema.foo.type, 'string');
+      assert.equal(barSchema.foo.foreignKey, true);
+      assert.equal(barSchema.foo.references, 'foo');
+      assert.equal(barSchema.foo.on, 'id');
     });
   });
 });
