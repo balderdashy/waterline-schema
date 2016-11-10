63e384e386c708911585fbee6a4ca86cfd73bcdc
Cody Stoltman <particlebanana@gmail.com>
2016-11-10 16:01:32 -0600

    rename attributes to schema to better reflect what it builds

---
+++ test/schema.js
@@ -0,0 +1,235 @@
+var SchemaBuilder = require('../lib/waterline-schema/schema');
+var assert = require('assert');
+
+describe('Schema Builder :: ', function() {
+
+  describe('Validating Identity', function() {
+    it('should throw an error when a collection is missing an identity', function() {
+      var collection = function() {};
+      collection.prototype = {
+        attributes: {
+          id: {
+            primaryKey: true,
+            type: 'number'
+          }
+        }
+      };
+
+      assert.throws(
+        function() {
+          SchemaBuilder([collection]);
+        }
+      );
+    });
+
+    it('should be valid when an identity is present', function() {
+      var collection = function() {};
+      collection.prototype = {
+        identity: 'FOO',
+        attributes: {
+          id: {
+            primaryKey: true,
+            type: 'number'
+          }
+        }
+      };
+
+      assert.doesNotThrow(
+        function() {
+          SchemaBuilder([collection]);
+        }
+      );
+    });
+
+    it('should allow tableName to suffice for identity', function() {
+      var collection = function() {};
+      collection.prototype = {
+        tableName: 'foo',
+        attributes: {
+          id: {
+            primaryKey: true,
+            type: 'number'
+          }
+        }
+      };
+
+      assert.doesNotThrow(
+        function() {
+          SchemaBuilder([collection]);
+        }
+      );
+    });
+  });
+
+
+  describe('Validating Primary Key', function() {
+    it('should enforce a primary key attribute to be included', function() {
+      var collection = function() {};
+      collection.prototype = {
+        identity: 'foo',
+        attributes: {}
+      };
+
+      assert.throws(
+        function() {
+          SchemaBuilder([collection]);
+        }
+      );
+    });
+
+    it('should allow the primary key to be set as a model option', function() {
+      var collection = function() {};
+      collection.prototype = {
+        identity: 'foo',
+        primaryKey: 'bar',
+        attributes: {
+          bar: {
+            type: 'string'
+          }
+        }
+      };
+
+      assert.doesNotThrow(
+        function() {
+          SchemaBuilder([collection]);
+        }
+      );
+    });
+
+    it('should allow the primary key to be set as a flag on an attribute', function() {
+      var collection = function() {};
+      collection.prototype = {
+        identity: 'foo',
+        attributes: {
+          bar: {
+            type: 'string'
+          },
+          baz: {
+            type: 'number',
+            primaryKey: true
+          }
+        }
+      };
+
+      assert.doesNotThrow(
+        function() {
+          SchemaBuilder([collection]);
+        }
+      );
+    });
+
+    it('should enforce a single primary key attribute', function() {
+      var collection = function() {};
+      collection.prototype = {
+        identity: 'foo',
+        attributes: {
+          bar: {
+            type: 'string',
+            primaryKey: true
+          },
+          baz: {
+            type: 'number',
+            primaryKey: true
+          }
+        }
+      };
+
+      assert.throws(
+        function() {
+          SchemaBuilder([collection]);
+        }
+      );
+    });
+  });
+
+
+  describe('Validating Instance Methods', function() {
+    it('should not allow instance methods', function() {
+      var collection = function() {};
+      collection.prototype = {
+        identity: 'foo',
+        primaryKey: 'id',
+        attributes: {
+          id: {
+            type: 'number'
+          },
+
+          getName: function() {}
+        }
+      };
+
+      assert.throws(
+        function() {
+          SchemaBuilder([collection]);
+        }
+      );
+    });
+  });
+
+
+  describe('Validating Attribute Properties', function() {
+    it('should not allow migration attributes', function() {
+      var collection = function() {};
+      collection.prototype = {
+        identity: 'foo',
+        primaryKey: 'id',
+        attributes: {
+          id: {
+            type: 'number',
+            unique: true
+          }
+        }
+      };
+
+      assert.throws(
+        function() {
+          SchemaBuilder([collection]);
+        }
+      );
+    });
+
+    it('should allow types', function() {
+      var collection = function() {};
+      collection.prototype = {
+        identity: 'foo',
+        primaryKey: 'id',
+        attributes: {
+          id: {
+            type: 'number'
+          }
+        }
+      };
+
+      assert.doesNotThrow(
+        function() {
+          SchemaBuilder([collection]);
+        }
+      );
+    });
+  });
+
+
+  describe('Validating Attribute Names', function() {
+    it('should not allow dots in attribute names', function() {
+      var collection = function() {};
+      collection.prototype = {
+        identity: 'foo',
+        primaryKey: 'id',
+        attributes: {
+          id: {
+            type: 'number'
+          },
+          'name.foo': {
+            type: 'number'
+          }
+        }
+      };
+
+      assert.throws(
+        function() {
+          SchemaBuilder([collection]);
+        }
+      );
+    });
+  });
+});
